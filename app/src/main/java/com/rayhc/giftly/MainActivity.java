fc0a512ece61d860b4c9f3b207e59c1076ef36c7
uhuruhashimoto <79157060+uhuruhashimoto@users.noreply.github.com>
2021-02-22 09:51:01 -0800

    make present button

--- app/src/main/java/com/rayhc/giftly/MainActivity.java
+++ app/src/main/java/com/rayhc/giftly/MainActivity.java
@@ -2,6 +2,8 @@ package com.rayhc.giftly;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -11,10 +13,12 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration myAppBarConfiguration;
+    FloatingActionButton actionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +38,14 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, myAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        actionButton = findViewById(R.id.fab);
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Create a new present", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         //starts login page
         Intent intent = new Intent(this, LoginActivity.class);
