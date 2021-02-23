12d728781212892e5fa67dc715f319fc8682394a
uhuruhashimoto <79157060+uhuruhashimoto@users.noreply.github.com>
2021-02-23 12:30:13 -0800

    gift constructor buttons

--- app/src/main/java/com/rayhc/giftly/CreateGiftFragment.java
+++ app/src/main/java/com/rayhc/giftly/CreateGiftFragment.java
@@ -1,5 +1,6 @@
 package com.rayhc.giftly;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,9 +10,42 @@ import android.widget.Button;
 import androidx.fragment.app.Fragment;
 
 public class CreateGiftFragment extends Fragment {
+    private Button linkButton, imageButton, videoButton;
+    private Gift newGift;
 
     public View onCreateView(LayoutInflater layoutInflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        return layoutInflater.inflate(R.layout.fragment_create_gift, container, false);
+        View v =  layoutInflater.inflate(R.layout.fragment_create_gift, container, false);
+        newGift = new Gift();
+        linkButton = getActivity().findViewById(R.id.link_button);
+        imageButton = getActivity().findViewById(R.id.image_button);
+        videoButton = getActivity().findViewById(R.id.video_button);
+         newGift.setTimeCreated(System.currentTimeMillis());
+
+         linkButton.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intent = new Intent();
+                 //start link activity
+             }
+         });
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                //start image activity
+            }
+        });
+
+        videoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                //start video activity
+            }
+        });
+
+        return v;
     }
 }
