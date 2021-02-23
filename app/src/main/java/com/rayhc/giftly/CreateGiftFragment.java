f79d342eb8e2c2497c299aed9c008bf2bb2e8cfb
uhuruhashimoto <79157060+uhuruhashimoto@users.noreply.github.com>
2021-02-23 12:17:06 -0800

    correcting Logan's atrociuos spelling

--- app/src/main/java/com/rayhc/giftly/CreateGiftFragment.java
+++ app/src/main/java/com/rayhc/giftly/CreateGiftFragment.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 public class CreateGiftFragment extends Fragment {
+
     public View onCreateView(LayoutInflater layoutInflater,
                              ViewGroup container, Bundle savedInstanceState) {
         return layoutInflater.inflate(R.layout.fragment_create_gift, container, false);
