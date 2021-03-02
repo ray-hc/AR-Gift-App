e476a5858ab23ea62694f9b0a6509feae4240627
Karim <karimitani9@hotmail.com>
2021-03-01 23:15:08 -0500

    Integrating UI with Friends

--- app/src/main/java/com/rayhc/giftly/FriendsFragment.java
+++ app/src/main/java/com/rayhc/giftly/FriendsFragment.java
@@ -14,11 +14,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.ListFragment;
 import androidx.preference.PreferenceManager;
 
@@ -33,19 +35,37 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-public class FriendsFragment extends ListFragment {
+public class FriendsFragment extends Fragment {
     private User activityUser;
     private ArrayList<String> friendsList;
+    private ArrayList<String> requestsList;
     private Context context;
+
+    private MyFriendsListAdapter friendsListAdapter;
+    private MyRequestsListAdapter requestsListAdapter;
+
+    private ListView friendsListView;
+    private ListView requestsListView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.friends_list_fragment, container, false);
 
-        Activity activity = this.getActivity();
+        friendsList = new ArrayList<String>();
+        requestsList = new ArrayList<String>();
 
         context = this.getActivity().getApplicationContext();
+
+        friendsListView = (ListView) view.findViewById(R.id.friends_list);
+        requestsListView = (ListView) view.findViewById(R.id.requests_list);
+
         getUserFromDB();
+
+        ListUtils.setDynamicHeight(friendsListView);
+        ListUtils.setDynamicHeight(requestsListView);
+
+        setHasOptionsMenu(true);
+
         return view;
     }
 
@@ -68,78 +88,153 @@ public class FriendsFragment extends ListFragment {
         }
     }
 
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-
-    }
-
     public void addFriend(String addedFriend) {
         UserManager.sendFriendRequest(activityUser, addedFriend);
     }
 
     public void getUserFromDB() {
-        DatabaseReference db = FirebaseDatabase.getInstance().getReference();
-
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
-        String displayUserID = sharedPref.getString("userId",null);
-
-        Query query = db.child("users").orderByChild("userId").equalTo(displayUserID);
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
+        Thread thread = new Thread(new Runnable() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()) {
-                    activityUser = UserManager.snapshotToUser(snapshot, displayUserID);
-                    ThreadLoader tl = new ThreadLoader();
-                    tl.start();
+            public void run() {
+                DatabaseReference db = FirebaseDatabase.getInstance().getReference();
 
-                }
-            }
+                SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+                String displayUserID = sharedPref.getString("userId",null);
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("kitani", "User ID: " + displayUserID);
 
+                Query query = db.child("users").orderByChild("userId").equalTo(displayUserID);
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()) {
+                            activityUser = UserManager.snapshotToUser(snapshot, displayUserID);
+
+                            if (activityUser == null){
+                                Log.d("kitani", "User object is null.");
+                            }
+
+                            if(activityUser.getFriends() != null){
+                                for(String key: activityUser.getFriends().keySet()) {
+                                    String friendID = activityUser.getFriends().get(key);
+                                    Query query = db.child("users").orderByChild("userId").equalTo(friendID);
+                                    query.addListenerForSingleValueEvent(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                            String friendName = (String) snapshot.child(friendID).child("name").getValue();
+                                            friendsList.add(friendName);
+                                        }
+
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError error) {
+                                        }
+                                    });
+                                }
+                            }
+                            Log.d("kitani", "Friends Added");
+
+                            if (activityUser.getReceivedFriends() != null){
+                                for(String key: activityUser.getReceivedFriends().keySet()) {
+                                    String requestID = activityUser.getReceivedFriends().get(key);
+                                    Query query = db.child("users").orderByChild("userId").equalTo(requestID);
+                                    query.addListenerForSingleValueEvent(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                            String friendName = (String) snapshot.child(requestID).child("name").getValue();
+                                            requestsList.add(friendName);
+                                        }
+
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError error) {
+                                        }
+                                    });
+                                }
+                            }
+                            Log.d("kitani", "Requests Added");
+
+
+                            friendsListAdapter = new MyFriendsListAdapter(context, R.layout.friend_entry, friendsList);
+                            requestsListAdapter = new MyRequestsListAdapter(context, R.layout.friend_request_entry, requestsList);
+
+                            friendsListView.setAdapter(friendsListAdapter);
+                            requestsListView.setAdapter(requestsListAdapter);
+
+                            Log.d("kitani", "Adapters Set");
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
             }
         });
+        thread.start();
     }
 
-    public class ThreadLoader extends Thread {
-        MyListAdapter adapter;
-        Runnable setAdapter = () -> setListAdapter(adapter);
-
-        public void run() {
-            String[] friendArray = activityUser.getFriends().values().toArray(new String[0]);
-
-            ArrayList<String> friends =  new ArrayList<String>();
-
-            for(String text:friendArray) {
-                friends.add(text);
-            }
-
-            adapter = new MyListAdapter(getActivity(), R.layout.friend_entry, friends);
-            getActivity().runOnUiThread(setAdapter);
-        }
-    }
-
-    public class MyListAdapter extends ArrayAdapter<String> {
+    public class MyFriendsListAdapter extends ArrayAdapter<String> {
         Context context;
+        ArrayList<String> f;
+        int res;
 
-        public MyListAdapter(Context c, int resource, ArrayList<String> friends) {
+        public MyFriendsListAdapter(Context c, int resource, ArrayList<String> friends) {
             super(c, resource, friends);
-            context = c;
+            this.context = c;
+            this.res = resource;
+            this.f = friends;
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             String friend = getResources().getStringArray(R.array.test_friends)[position];
-
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.friend_entry, parent, false);
-
+            convertView = LayoutInflater.from(getContext()).inflate(res, parent, false);
+            Log.d("kitani", "Friend: " + friend );
             TextView friendName = (TextView) convertView.findViewById(R.id.friend);
-
             friendName.setText(friend);
 
             return convertView;
         }
+    }
 
+    public class MyRequestsListAdapter extends ArrayAdapter<String> {
+        Context context;
+        ArrayList<String> r;
+        int res;
+
+        public MyRequestsListAdapter(Context c, int resource, ArrayList<String> requests) {
+            super(c, resource, requests);
+            this.context = c;
+            this.res = resource;
+            this.r = requests;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            String request = getResources().getStringArray(R.array.test_friends)[position];
+            convertView = LayoutInflater.from(getContext()).inflate(res, parent, false);
+
+            TextView friendName = (TextView) convertView.findViewById(R.id.friend_request);
+            friendName.setText(request);
+
+            Button add = (Button) convertView.findViewById(R.id.add_friend);
+            Button decline = (Button) convertView.findViewById(R.id.decline_button);
+
+            add.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+            decline.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+            return convertView;
+        }
     }
 }
--- app/src/main/java/com/rayhc/giftly/ReviewGiftActivity.java
+++ app/src/main/java/com/rayhc/giftly/ReviewGiftActivity.java
@@ -105,29 +105,4 @@ public class ReviewGiftActivity extends AppCompatActivity {
         ListUtils.setDynamicHeight(mMediaList);
     }
 
-
-    /**
-     * Util class so the listviews all fit
-     */
-    public static class ListUtils {
-        public static void setDynamicHeight(ListView mListView) {
-            ListAdapter mListAdapter = mListView.getAdapter();
-            if (mListAdapter == null) {
-                // when adapter is null
-                return;
-            }
-            int height = 0;
-            int desiredWidth = View.MeasureSpec.makeMeasureSpec(mListView.getWidth(), View.MeasureSpec.UNSPECIFIED);
-            for (int i = 0; i < mListAdapter.getCount(); i++) {
-                View listItem = mListAdapter.getView(i, null, mListView);
-                listItem.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
-                height += listItem.getMeasuredHeight();
-            }
-            ViewGroup.LayoutParams params = mListView.getLayoutParams();
-            params.height = height + (mListView.getDividerHeight() * (mListAdapter.getCount() - 1));
-            mListView.setLayoutParams(params);
-            mListView.requestLayout();
-        }
-    }
-
 }
